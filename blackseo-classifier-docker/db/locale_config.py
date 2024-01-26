7ba7173a9f61490adf3aab73fc859225d9796185
mww@sp <mww@sp>
2024-01-26 16:44:20 +0800

    all classifiers ready

---
+++ blackseo-classifier-docker/db/locale_config.py
@@ -0,0 +1,11 @@
+root = '/path/to/regseo/blackseo-classifier-docker/'  # sp服务器上的版本
+
+default = {
+    "keyword-url": root + 'db/keyword-url.json',
+    "keyword-word": root + 'db/keyword-word.json',
+    "new-stopword": root + 'db/new-stopword.json',
+    "keyword-ibt": root + 'db/keyword-ibt.json',
+    "keyword-plat": root + 'db/keyword-plat.json',
+    "keyword-media": root + 'db/keyword-media.json'
+    #"default-download": './dl/',
+}
